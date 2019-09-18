d897609613ac0a7faedf050234e0bf217a9ed0ca
github username <github email address>
2019-09-18 15:21:03 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def initialilize(name, breed)
+        @name = name
+        @breed = breed
+    end
+    def name=(name)
+        @name = name
+    end
+    def breed=(breed)
+        @breed = breed
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
+end
